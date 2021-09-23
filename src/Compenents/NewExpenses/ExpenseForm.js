7ce2469f13c32412119fb93142f1719963017e2d
idris <idrisdoyin@gmail.com>
2021-09-23 19:05:47 +0100

    'handle_input'

--- src/Compenents/NewExpenses/ExpenseForm.js
+++ src/Compenents/NewExpenses/ExpenseForm.js
@@ -6,17 +6,26 @@ const ExpenseForm = (props) => {
 	const [ enteredAmount, setEnteredAmount ] = useState('');
 	const [ enteredDate, setEnteredDate ] = useState('');
 	const [ isValidTitle, setIsValidTitle ] = useState(true);
-	const [ isValidAmoutn, setIsValidAmount ] = useState(true);
+	const [ isValidAmount, setIsValidAmount ] = useState(true);
 	const [ isValidDate, setIsValidDate ] = useState(true);
 
 	const titleChangeHandler = (event) => {
-		setEnteredTitle(event.target.value);
+		if (event.target.value.trim().length > 0) {
+			setIsValidTitle(true);
+			setEnteredTitle(event.target.value);
+		}
 	};
 	const newAmountHandler = (event) => {
-		setEnteredAmount(event.target.value);
+		if (event.target.value.trim().length > 0) {
+			setIsValidAmount(true);
+			setEnteredAmount(event.target.value);
+		}
 	};
 	const dateChangeHandler = (event) => {
-		setEnteredDate(event.target.value);
+		if (event.target.value.toString().trim().length > 0) {
+			setIsValidDate(true);
+			setEnteredDate(event.target.value);
+		}
 	};
 	const handleSubmit = (event) => {
 		event.preventDefault();
@@ -25,20 +34,34 @@ const ExpenseForm = (props) => {
 			amount: enteredAmount,
 			date: new Date(enteredDate)
 		};
-		if (expenseData.title.trim().length === 0) {
+		// if (enteredTitle.trim().length === 0) {
+		// 	setIsValidTitle(false);
+		// } else if (enteredAmount.trim().length === 0) {
+		// 	setIsValidAmount(false);
+		// } else if (enteredDate.date.toString().trim().length === 0) {
+		// 	setIsValidDate(false);
+		// } else {
+
+		// 	setIsValidTitle(true);
+		// 	setIsValidDate(true);
+		// 	setIsValidAmount(true);
+
+		// }
+		if (enteredTitle.trim().length === 0) {
 			setIsValidTitle(false);
-		} else if (expenseData.amount.trim().length === 0) {
+		} else if (enteredAmount.trim().length === 0) {
 			setIsValidAmount(false);
-		} else if (expenseData.date.toString().trim().length === 0) {
+		} else if (enteredDate.trim().length === 0) {
 			setIsValidDate(false);
+		} else {
+			setIsValidTitle(true);
+			setIsValidAmount(true);
+			setIsValidDate(true);
+			props.onExpensesSubmit(expenseData);
+			setEnteredTitle('');
+			setEnteredAmount('');
+			setEnteredDate('');
 		}
-		props.onExpensesSubmit(expenseData);
-		setIsValidTitle(true);
-		setIsValidDate(true);
-		setIsValidAmount(true);
-		setEnteredTitle('');
-		setEnteredAmount('');
-		setEnteredDate('');
 	};
 
 	return (
@@ -52,11 +75,17 @@ const ExpenseForm = (props) => {
 						value={enteredTitle}
 						className={` ${!isValidTitle ? 'incorrect_label' : 'correct_label'}`}
 					/>
-					{!isValidTitle ? <h6 className="field_required">incorrect_label</h6> : ''}
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={newAmountHandler} />
+					<input
+						type="number"
+						min="0.01"
+						step="0.01"
+						value={enteredAmount}
+						onChange={newAmountHandler}
+						className={` ${!isValidAmount ? 'incorrect_label' : 'correct_label'}`}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
@@ -66,6 +95,7 @@ const ExpenseForm = (props) => {
 						max="2022-12-31"
 						value={enteredDate}
 						onChange={dateChangeHandler}
+						className={` ${!isValidDate ? 'incorrect_label' : 'correct_label'}`}
 					/>
 				</div>
 			</div>
